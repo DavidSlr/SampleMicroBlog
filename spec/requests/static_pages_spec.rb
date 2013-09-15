a0aaa813b5149886dbebabee3c8c49656ab254ee
David Schneller <dschneller+git@gmail.com>
2013-09-15 16:19:57 +0200

    Add static page, title in app theme and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,14 +4,14 @@ describe "StaticPages" do
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', :text => 'Sample App')
 		end
 	end
 
 	describe "About page" do
 		it "should have the content 'About us'" do
 			visit '/static_pages/about'
-			page.should have_content('About us')
+			page.should have_selector('h1', :text => 'About us')
 		end
 	end
 end
